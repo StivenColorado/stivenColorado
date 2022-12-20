c887768f28c31aaf52b7908c14f4c65f50ab1023
Stiven Colorado <90488682+StivenColorado@users.noreply.github.com>
2022-12-20 12:35:21 -0500

    Add files via upload

--- js/mouse.js
+++ js/mouse.js
@@ -1,45 +1,16 @@
-function main2()
-{
-        let cursor = document.querySelector('.cursor')
-        let text = document.querySelector('section.contenedores')
-        let secondContainer = document.querySelector('div.container2')
-        let thirdContainer = document.querySelector('div.container3')
-        let lastContainer = document.querySelector('div.container4') 
-
-        let bandera = 'v'
-    window.addEventListener('mousemove', (e) => {
-        //console.log(e.pageX , e.pageY)
-        if(bandera=='v')
-        {
-            cursor.style.backgroundColor = 'rgb(255,255,255,1)'
-            cursor.style.color = 'black';
-        }else{
-            cursor.style.backgroundColor = 'red'
-            cursor.style.color = 'rgb(255,255,255,1)'
-        }
-        cursor.style.top = (e.pageY - 20) +'px'
-        cursor.style.left = (e.pageX - 20) +'px'
-            
-            bandera = 'v'
-    })
-    text.addEventListener('mousemove', () =>
-    {
-
-        bandera = 'f'
-    })
-    secondContainer.addEventListener('mousemove', () =>
-    {
-        bandera = 'f'
-    })
-
-    thirdContainer.addEventListener('mousemove', () =>
-    {
-        bandera='f'
-    })
-
-    lastContainer.addEventListener('mousemove', () =>
-    {
-        bandera='f'
-    })
-    
-}
+function main2(){let cursor=document.querySelector('.cursor')
+let text=document.querySelector('section.contenedores')
+let secondContainer=document.querySelector('div.container2')
+let thirdContainer=document.querySelector('div.container3')
+let lastContainer=document.querySelector('div.container4')
+let bandera='v'
+window.addEventListener('mousemove',(e)=>{if(bandera=='v'){cursor.style.backgroundColor='rgb(255,255,255,1)'
+cursor.style.color='black'}else{cursor.style.backgroundColor='red'
+cursor.style.color='rgb(255,255,255,1)'}
+cursor.style.top=(e.pageY-20)+'px'
+cursor.style.left=(e.pageX-20)+'px'
+bandera='v'})
+text.addEventListener('mousemove',()=>{bandera='f'})
+secondContainer.addEventListener('mousemove',()=>{bandera='f'})
+thirdContainer.addEventListener('mousemove',()=>{bandera='f'})
+lastContainer.addEventListener('mousemove',()=>{bandera='f'})}
