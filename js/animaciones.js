7bc8bb6a6c8dac37f112765555a0a0232d8c6f44
Stiven Colorado <90488682+StivenColorado@users.noreply.github.com>
2022-12-23 20:23:26 -0500

    Add files via upload

--- js/animaciones.js
+++ js/animaciones.js
@@ -1,44 +1,64 @@
-function animaciones(){let pantalla=window.innerWidth;window.addEventListener('scroll',()=>{let barras=document.getElementsByClassName('colorBars')
-let secciones=document.getElementsByClassName('secciones')
-let contenedorContacto=document.getElementsByClassName('contentContainer4')
-let skill=document.getElementById('skills')
-let skill2=document.getElementById('textSkills')
-let design=document.getElementById('contentContact')
-let pos=skill.getBoundingClientRect().top;let pos2=skill2.getBoundingClientRect().top;let pos3=design.getBoundingClientRect().top;if(pantalla>750){if(pos<=600){for(i=0;i<barras.length;i++){barras[i].style.animation='barrasColores 500ms alternate forwards'}
-for(i=0;i<secciones.length;i++){secciones[i].style.animation='deslizarSkillMostrar 1s alternate forwards'}}
-if(pos3>600){design.style.animation='designClipShow 2s alternate forwards'}
-if(pos>600){for(i=0;i<barras.length;i++){barras[i].style.animation='barrasSinColores 500ms alternate forwards'}
-for(i=0;i<secciones.length;i++){secciones[i].style.animation='deslizarSkillOcultar 1s alternate forwards'}}}
-if(pos3>990){design.style.animation='designClipHidde 2s alternate forwards'}
-if(pantalla<750){if(pos<=600){for(i=0;i<barras.length;i++){barras[i].style.animation='barrasColores 800ms alternate forwards'}
-for(i=0;i<secciones.length;i++){secciones[i].style.animation='deslizarSkillMostrar 1s alternate forwards'}}
-if(pos3>600){design.style.animation='designClipShow 2s alternate forwards'}
-if(pos>600){for(i=0;i<barras.length;i++){barras[i].style.animation='barrasSinColores 500ms alternate forwards'}
-for(i=0;i<secciones.length;i++){secciones[i].style.animation='deslizarSkillOcultar 1s alternate forwards'}}
-if(pos3>600){design.style.animation='designClipShow 2s alternate backwards'}}})
-let contador=0;let btnNav=document.getElementById('open')
-let btnNav2=document.getElementById('closeNav')
-let alternar=document.getElementsByClassName('opcionesPhone')
-btnNav.onclick=function(){abrirMenu()}
-btnNav2.onclick=function(){cerrarMenu()}
-alternar[0].onclick=function(){cerrarMenu()}
-alternar[1].onclick=function(){cerrarMenu()}
-alternar[2].onclick=function(){cerrarMenu()}
-alternar[3].onclick=function(){cerrarMenu()}}
-let prueba=function(e){e.style.animation='moveCloud 500ms alternate forwards'}
-let abrirMenu=function(){let menu=document.getElementById('todo')
-let opciones=document.getElementById('fullnav')
-let btnNav=document.getElementById('open')
-let btnNav2=document.getElementById('closeNav')
-menu.style.display='flex'
-menu.style.animation='openNav 500ms alternate forwards'
-opciones.style.animation='deslizarAbajoOpciones 1s alternate forwards'
-opciones.style.animationDelay='50ms'
-btnNav2.style.animation='virarClose 2s alternate forwards'}
-let cerrarMenu=function(){let menu=document.getElementById('todo')
-let opciones=document.getElementById('fullnav')
-let btnNav=document.getElementById('open')
-let btnNav2=document.getElementById('closeNav')
-menu.style.animation='closeNav 3s alternate forwards'
-opciones.style.animation='deslizarArribaOpciones 500ms alternate forwards'
-btnNav2.style.animation='virarOpen 2s alternate forwards'}
+function animaciones() {
+    let pantalla = window.innerWidth; window.addEventListener('scroll', () => {
+        let barras = document.getElementsByClassName('colorBars')
+        let secciones = document.getElementsByClassName('secciones')
+        let contenedorContacto = document.getElementsByClassName('contentContainer4')
+        let skill = document.getElementById('skills')
+        let skill2 = document.getElementById('textSkills')
+        let design = document.getElementById('contentContact')
+        let pos = skill.getBoundingClientRect().top; let pos2 = skill2.getBoundingClientRect().top; let pos3 = design.getBoundingClientRect().top; if (pantalla > 750) {
+            if (pos <= 600) {
+                for (i = 0; i < barras.length; i++) { barras[i].style.animation = 'barrasColores 500ms alternate forwards' }
+                for (i = 0; i < secciones.length; i++) { secciones[i].style.animation = 'deslizarSkillMostrar 1s alternate forwards' }
+            }
+            if (pos3 > 600) { design.style.animation = 'designClipShow 2s alternate forwards' }
+            if (pos > 600) {
+                for (i = 0; i < barras.length; i++) { barras[i].style.animation = 'barrasSinColores 500ms alternate forwards' }
+                for (i = 0; i < secciones.length; i++) { secciones[i].style.animation = 'deslizarSkillOcultar 1s alternate forwards' }
+            }
+        }
+        if (pos3 > 990) { design.style.animation = 'designClipHidde 2s alternate forwards' }
+        if (pantalla < 750) {
+            if (pos <= 600) {
+                for (i = 0; i < barras.length; i++) { barras[i].style.animation = 'barrasColores 800ms alternate forwards' }
+                for (i = 0; i < secciones.length; i++) { secciones[i].style.animation = 'deslizarSkillMostrar 1s alternate forwards' }
+            }
+            if (pos3 > 600) { design.style.animation = 'designClipShow 2s alternate forwards' }
+            if (pos > 600) {
+                for (i = 0; i < barras.length; i++) { barras[i].style.animation = 'barrasSinColores 500ms alternate forwards' }
+                for (i = 0; i < secciones.length; i++) { secciones[i].style.animation = 'deslizarSkillOcultar 1s alternate forwards' }
+            }
+            if (pos3 > 600) { design.style.animation = 'designClipShow 2s alternate backwards' }
+        }
+    })
+    let contador = 0; let btnNav = document.getElementById('open')
+    let btnNav2 = document.getElementById('closeNav')
+    let alternar = document.getElementsByClassName('opcionesPhone')
+    btnNav.onclick = function () { abrirMenu() }
+    btnNav2.onclick = function () { cerrarMenu() }
+    alternar[0].onclick = function () { cerrarMenu() }
+    alternar[1].onclick = function () { cerrarMenu() }
+    alternar[2].onclick = function () { cerrarMenu() }
+    alternar[3].onclick = function () { cerrarMenu() }
+}
+let prueba = function (e) { e.style.animation = 'moveCloud 500ms alternate forwards' }
+let abrirMenu = function () {
+    let menu = document.getElementById('todo')
+    let opciones = document.getElementById('fullnav')
+    let btnNav = document.getElementById('open')
+    let btnNav2 = document.getElementById('closeNav')
+    menu.style.display = 'flex'
+    menu.style.animation = 'openNav 500ms alternate forwards'
+    opciones.style.animation = 'deslizarAbajoOpciones 1s alternate forwards'
+    opciones.style.animationDelay = '50ms'
+    btnNav2.style.animation = 'virarClose 2s alternate forwards'
+}
+let cerrarMenu = function () {
+    let menu = document.getElementById('todo')
+    let opciones = document.getElementById('fullnav')
+    let btnNav = document.getElementById('open')
+    let btnNav2 = document.getElementById('closeNav')
+    menu.style.animation = 'closeNav 3s alternate forwards'
+    opciones.style.animation = 'deslizarArribaOpciones 500ms alternate forwards'
+    btnNav2.style.animation = 'virarOpen 2s alternate forwards'
+}
--- js/move.js
+++ js/move.js
@@ -1,30 +1,46 @@
-function main(){const cajas=document.getElementsByClassName('contenedores')
-let contador=0;let b="f"
-let vueltas=1
-let elementsAmount=cajas.length-1
-cajas[0].style.display='flex'
-function ocultar(){if(vueltas<=9){if(contador>0){cajas[contador-1].style.display='none'
-cajas[contador-1].style.backgroundColor='none'}else if(b=="v"){cajas[elementsAmount-1].style.display='none'}
-cajas[contador].style.display='flex'
-contador+=1
-if(contador==elementsAmount){contador=0
-b="v"}
-vueltas+=1}}
-if(contador<=elementsAmount){let tiempo=setInterval(()=>{ocultar()},2000)}
-let box=document.getElementsByClassName('boxSlider')
-let slider=document.getElementsByClassName('allSliders')
-amount=slider.length
-let top=150
-let bottom=0
-let contado=0
-setInterval(()=>{if(contado<=2){slider[0].style.transform=`translateY(-${top}px)`
-slider[0].style.animationFillMode='forwards'
-slider[0].style.transition='linear 1s'
-top+=150
-contado=contado+1
-if(contado==3){top=0
-slider[0].style.transform=`translateY(-${top}px)`
-contado=-1}}
-let screen=window.innerWidth},6000);const btn=document.getElementsByClassName('openNav')
-btn[0].onclick=function(){pressBtn(btn[0])}}
-let pressBtn=function(e){e.style.animation='btnNav 500ms linear forwards'}
+function main() {
+    const cajas = document.getElementsByClassName('contenedores')
+    let contador = 0; let b = "f"
+    let vueltas = 1
+    let elementsAmount = cajas.length - 1
+    cajas[0].style.display = 'flex'
+    function ocultar() {
+        if (vueltas <= 9) {
+            if (contador > 0) {
+                cajas[contador - 1].style.display = 'none'
+                cajas[contador - 1].style.backgroundColor = 'none'
+            } else if (b == "v") { cajas[elementsAmount - 1].style.display = 'none' }
+            cajas[contador].style.display = 'flex'
+            contador += 1
+            if (contador == elementsAmount) {
+                contador = 0
+                b = "v"
+            }
+            vueltas += 1
+        }
+    }
+    if (contador <= elementsAmount) { let tiempo = setInterval(() => { ocultar() }, 2000) }
+    let box = document.getElementsByClassName('boxSlider')
+    let slider = document.getElementsByClassName('allSliders')
+    amount = slider.length
+    let top = 150
+    let bottom = 0
+    let contado = 0
+    setInterval(() => {
+        if (contado <= 2) {
+            slider[0].style.transform = `translateY(-${top}px)`
+            slider[0].style.animationFillMode = 'forwards'
+            slider[0].style.transition = 'linear 1s'
+            top += 150
+            contado = contado + 1
+            if (contado == 3) {
+                top = 0
+                slider[0].style.transform = `translateY(-${top}px)`
+                contado = -1
+            }
+        }
+        let screen = window.innerWidth
+    }, 6000); const btn = document.getElementsByClassName('openNav')
+    btn[0].onclick = function () { pressBtn(btn[0]) }
+}
+let pressBtn = function (e) { e.style.animation = 'btnNav 500ms linear forwards' }
