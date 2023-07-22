c15f6fc64266b20408bb69c5a68cb29a17710f26
Stiven Colorado <90488682+StivenColorado@users.noreply.github.com>
2023-07-21 23:45:36 -0500

    adding information about projects

--- js/slider.js
+++ js/slider.js
@@ -3,14 +3,24 @@
 const imgSlider = document.querySelector('.img-slider');
 const pagination = document.querySelector('.radios');
 const imagenes = [
-    './img/portafolio/picture (1).png',
-    './img/portafolio/picture (2).png',
-    './img/portafolio/picture (3).png',
-    './img/portafolio/picture (4).png',
-    './img/portafolio/picture (5).png',
-    './img/portafolio/picture (6).png',
-    './img/portafolio/picture (7).png'
-  ];
+  './img/portafolio/picture (1).png',
+  './img/portafolio/picture (2).png',
+  './img/portafolio/picture (3).png',
+  './img/portafolio/picture (4).png',
+  './img/portafolio/picture (5).png',
+  './img/portafolio/picture (6).png',
+  './img/portafolio/picture (7).png',
+  // 6
+  './img/portafolio/OnlineStore/editProfile.png',
+  './img/portafolio/OnlineStore/addCart.png',
+  './img/portafolio/OnlineStore/cart.png',
+  './img/portafolio/OnlineStore/items.png',
+  './img/portafolio/OnlineStore/management.png',
+  './img/portafolio/OnlineStore/rol.png',
+  './img/portafolio/OnlineStore/uploadImage.png',
+  './img/portafolio/OnlineStore/profile.png'
+  // './img/portafolio/test.mp4'
+];
 
 let indiceImagen = 0;
 let dots = [];
@@ -20,7 +30,26 @@ function cambiarImagen(indice) {
   dots.forEach(dot => dot.classList.remove('active'));
   dots[indice].classList.add('active');
   indiceImagen = indice;
+
+  // Obtener los elementos del título y descripción del slider
+  const sliderTitle = document.getElementById('slider-title');
+  const sliderDescription = document.getElementById('slider-description');
+
+  // Verificar si el índice está en el rango de imágenes específicas
+  if (indice >= 6 && indice < imagenes.length) {
+    // Restaurar el contenido original del título y descripción
+    sliderTitle.textContent = 'online jacket shop simulation';
+    sliderDescription.textContent =
+      `Welcome to our online simulation of a premium sack store! Experience an immersive shopping journey with cutting - edge features, including inventory management, personalized sack customization, and virtual store designing.Attract customers and boost profits with our smart pricing strategies.Explore a diverse collection of sacks, manage stock efficiently, and design your dream store in this fashion - forward software.Get ready for a one - of - a - kind virtual shopping experience that seamlessly blends fashion and technology.
+  `} else {
+    // Restaurar el contenido original del título y descripción
+    sliderTitle.textContent = 'online cap shop simulation';
+    sliderDescription.textContent = 'Our online hat shop simulation program offers a wide range of functionalities for an immersive and enriching experience. We highlight inventory management, product customization, and virtual store design. We also offer pricing strategies to attract customers and maximize profits.';
+  }
 }
+
+// Resto del código del slider...
+
 
 imagenes.forEach((imagen, indice) => {
   const dot = document.createElement('span');
@@ -51,6 +80,7 @@ btnBack.addEventListener('click', () => {
   }
   cambiarImagen(indiceImagen);
 });
+
 
 setInterval(() => {
   indiceImagen++;
